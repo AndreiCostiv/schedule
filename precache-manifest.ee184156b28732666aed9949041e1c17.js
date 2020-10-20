cd3088d84534ce6f8a5633a859ba516f45bbecbe
Andrii <costivandrei@gmail.com>
2020-10-20 22:49:46 +0100

    Updates

---
+++ precache-manifest.ee184156b28732666aed9949041e1c17.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2779197e4fd20b34184c14b903cf430",
+    "url": "/schedule/index.html"
+  },
+  {
+    "revision": "a0097bb5c625497c3d07",
+    "url": "/schedule/static/css/main.cf037ef9.chunk.css"
+  },
+  {
+    "revision": "cda1c3f28943aaf0a7af",
+    "url": "/schedule/static/js/2.167b7215.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/schedule/static/js/2.167b7215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0097bb5c625497c3d07",
+    "url": "/schedule/static/js/main.92a7184a.chunk.js"
+  },
+  {
+    "revision": "9769cfa4dffd2da8f3ef",
+    "url": "/schedule/static/js/runtime-main.113be91a.js"
+  },
+  {
+    "revision": "7b6f6b4689e824c53b1da3f69040b7da",
+    "url": "/schedule/static/media/AboutUs.7b6f6b46.svg"
+  },
+  {
+    "revision": "47d204fb8c12383ab035916af54bbaab",
+    "url": "/schedule/static/media/DragIcon.47d204fb.svg"
+  },
+  {
+    "revision": "dec8cad7d2ff640f6eeebe203e214f85",
+    "url": "/schedule/static/media/NewTask.dec8cad7.svg"
+  },
+  {
+    "revision": "e16637bf0bf2317f1af91d5e9b5dc123",
+    "url": "/schedule/static/media/RemoveIcon.e16637bf.svg"
+  },
+  {
+    "revision": "7c18188784f21915f42a5b3bc9d91e20",
+    "url": "/schedule/static/media/Roboto-Bold.7c181887.ttf"
+  },
+  {
+    "revision": "c6cdfded4630ba6d9a2dceb70aa4fe0f",
+    "url": "/schedule/static/media/Roboto-Light.c6cdfded.ttf"
+  },
+  {
+    "revision": "d52f011be65b281ba8ca1c3f689cf133",
+    "url": "/schedule/static/media/Roboto-Medium.d52f011b.ttf"
+  },
+  {
+    "revision": "18d44f79b3979ec168862093208c6d7d",
+    "url": "/schedule/static/media/Roboto-Regular.18d44f79.ttf"
+  },
+  {
+    "revision": "27f7ef718eb424d07e4d443a5e116b3f",
+    "url": "/schedule/static/media/Settings.27f7ef71.svg"
+  },
+  {
+    "revision": "b2666e265afd47988d86813af97f4cd8",
+    "url": "/schedule/static/media/Sign_Out.b2666e26.svg"
+  },
+  {
+    "revision": "8e35e5197a600e23af557a2f0620e904",
+    "url": "/schedule/static/media/ToDo.8e35e519.svg"
+  }
+]);
